bd648ca9f8a2d556196a06e60a5c350e0234bb99
MatthewBazinet <matthew.r.bazinet@gmail.com>
2021-02-14 20:39:48 -0500

    Triangle and Mesh, Model and GameObject

--- Game/Scene/GameScene.cpp
+++ Game/Scene/GameScene.cpp
@@ -1,16 +1,34 @@
 #include "GameScene.h"
 
-GameScene::GameScene()
+GameScene::GameScene() : shape(nullptr)
 {
 }
 
 GameScene::~GameScene()
 {
+	delete shape;
+	shape = nullptr;
 }
 
 bool GameScene::OnCreate()
 {
 	Log::Info("Game Scene initiated", "GameScene.cpp", __LINE__);
+
+	Vertex v;
+	std::vector<Vertex> vertexList;
+	vertexList.reserve(3);
+	v.position = glm::vec3(0.0f, 0.5f, 0.0f);
+	vertexList.push_back(v);
+	v.position = glm::vec3(-0.5f, -0.5f, 0.0f);
+	vertexList.push_back(v);
+	v.position = glm::vec3(0.5f, -0.5f, 0.0f);
+	vertexList.push_back(v);
+
+
+	Model* model = new Model();
+	model->AddMesh(new Mesh(vertexList));
+	shape = new GameObject(model);
+
 	return true;
 }
 
@@ -20,4 +38,5 @@ void GameScene::Update(const float deltaTime_)
 
 void GameScene::Render()
 {
+	shape->Render();
 }
