3aad92ce52071f638012207b56ee804350be1b26
MatthewBazinet <matthew.r.bazinet@gmail.com>
2021-09-16 15:43:11 -0400

    Reupload of Character.h

---
+++ Game/Character.h
@@ -0,0 +1,33 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include "../Engine/Rendering/3D/GameObject.h"
+
+class Character : GameObject
+{
+public:
+	void QCF(int strength, bool simpleInput);
+	void QCB(int strength, bool simpleInput);
+	void Unique();
+	void Light();
+	void Medium();
+	void Heavy();
+	void Run(bool isBegin);
+
+	void Move(glm::vec2 input);
+
+private:
+	void AirQCF(int strength, bool simpleInput);
+	void AirQCB(int strength, bool simpleInput);
+	void AirUnique();
+	void AirLight();
+	void AirMedium();
+	void AirHeavy();
+
+
+
+	float health;
+	float meter;
+};
+
+#endif
