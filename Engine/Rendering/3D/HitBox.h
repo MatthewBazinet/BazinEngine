b3f41aef7ae0ae23ce5336ba944523a31003a3f4
TheBenBro <54993958+TheBenBro@users.noreply.github.com>
2021-09-29 15:37:18 -0400

    Added Hitbox class which will be used for spawning and despawning bounding boxes

---
+++ Engine/Rendering/3D/HitBox.h
@@ -0,0 +1,15 @@
+#ifndef HITBOX_H
+#define HITBOX_H
+#include "../../Math/BoundingBox.h"
+class HitBox
+{
+public:
+	HitBox();
+	~HitBox();
+
+	void SpawnHitBox();
+	void SpawnHurtBox();
+private:
+	
+};
+#endif
