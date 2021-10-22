0e5c90081ae53dfaef56f0dc69a1fe93f2f9ccee
TheBenBro <54993958+TheBenBro@users.noreply.github.com>
2021-10-22 15:48:08 -0400

    Fixed some issues that prevented the project to compile becuase of my code.

--- Engine/Core/GameInterface.h
+++ Engine/Core/GameInterface.h
@@ -1,6 +1,10 @@
 #ifndef GAMEINTERFACE_H
 #define GAMEINTERFACE_H
 
+#include "imgui.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_sdl.h"
+#include "..//Events/EventListener.h"
 class GameInterface
 {
 public:
@@ -8,11 +12,11 @@ public:
 	virtual ~GameInterface() {}
 
 	virtual bool OnCreate() = 0;
-	virtual void Update(const float delataTime_) = 0;
+	virtual void Update(const float deltaTime_) = 0;
 	virtual void Render() = 0;
-	virtual void HandleEvents(const SDL_Event& sdlEvent) = 0;
-	virtual void NotifyOfKeyDown(const SDL_Scancode key_) = 0;
-	virtual void NotifyOfKeyUp(const SDL_Scancode key_) = 0;
+	void NotifyOfKeyDown(const SDL_Scancode key_);
+	void NotifyOfKeyUp(const SDL_Scancode key_);
+private:
 };
 #endif
 
