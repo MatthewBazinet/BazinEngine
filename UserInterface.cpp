add21a1c65b46b88d8fced24651a07ffa5d0ba64
DESKTOP-U3M6GGI\Kai <kaibro224@gmail.com>
2021-12-11 11:06:21 -0500

    Xml reader for decision trees

--- UserInterface.cpp
+++ UserInterface.cpp
@@ -71,7 +71,7 @@ void UserInterface::Render()
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 	}
-	ImGui_ImplOpenGL3_DestroyDeviceObjects();
+	//ImGui_ImplOpenGL3_DestroyDeviceObjects();
 }
 
 void UserInterface::StartTimer()
@@ -141,11 +141,10 @@ void UserInterface::ShowMenu()
 //Ui for in game
 void UserInterface::ShowGameUi()
 {
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplSDL2_NewFrame();
+	ImGui::NewFrame();
 	if (state == State::SinglePlayer) {
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplSDL2_NewFrame();
-		ImGui::NewFrame();
-
 		//Player 1 Health Bar
 		ImGui::SetNextWindowPos(ImVec2(CoreEngine::GetInstance()->GetScreenWidth() * 0.75f, CoreEngine::GetInstance()->GetScreenHeight() * 0.05f), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
 		ImGui::Begin("Player 2", NULL, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoMouseInputs);
--- XMLDecisionTreeReader.cpp
+++ XMLDecisionTreeReader.cpp
@@ -3,11 +3,78 @@
 DecisionTreeNode XMLDecisionTreeReader::ReadFile(const char* filepath_)
 {
 	XMLDocument doc;
-	//this parses the xml file allowing you to read the values inside of it.
 	doc.LoadFile(filepath_);
-	//this is the root node
-	XMLElement* decisionNode = doc.FirstChildElement(" ");
-	//this is the child of the root node
-	XMLElement* branch = decisionNode->FirstChildElement(" ");
-	return DecisionTreeNode();
+	bool status = doc.Error();
+	if (status) {
+		std::cout << doc.ErrorIDToName(doc.ErrorID()) << std::endl;
+	}
+	std::vector<XMLElement*> tmp_decisionNode;
+	std::vector<std::string> decisionNode;
+	XMLDecision decision;
+	tmp_decisionNode[0] = doc.FirstChildElement("decisionTree");
+	std::vector<XMLElement*> branchT;
+	std::vector<XMLElement*> branchF;
+	DecisionTreeNode* tmpT;
+	DecisionTreeNode* tmpF;
+	int counter = 1;
+	int i = 0;
+	if (tmp_decisionNode[i]->FirstChild() == tmp_decisionNode[i]->FirstChildElement("test")) {
+		tmp_decisionNode[i + 1] = tmp_decisionNode[i]->FirstChildElement("test");//first test node
+		decisionNode.push_back(tmp_decisionNode[i]->FirstChildElement("test")->GetText());
+		//decision[i]->SetTrueNode(tmpT);
+		//decision[i]->SetFalseNode(tmpF);
+		branchT[i] = tmp_decisionNode[i]->FirstChildElement("true");
+		branchF[i] = tmp_decisionNode[i]->FirstChildElement("false");
+		tmpT = nullptr;
+		tmpF = nullptr;
+	}
+
+	while (true) {
+
+		if (tmp_decisionNode[i]->FirstChild() == tmp_decisionNode[i]->FirstChildElement("test")) {
+			tmp_decisionNode[i + 1] = tmp_decisionNode[i]->FirstChildElement("test");//first test node
+			decisionNode.push_back(tmp_decisionNode[i]->FirstChildElement("test")->GetText());
+			//decision[i]->SetTrueNode(tmpT);
+			//decision[i]->SetFalseNode(tmpF);
+			branchT[i] = tmp_decisionNode[i]->FirstChildElement("true");
+			branchF[i] = tmp_decisionNode[i]->FirstChildElement("false");
+			tmpT = nullptr;
+			tmpF = nullptr;
+		}else if (branchT[i]->FirstChild() == branchT[i]->FirstChildElement("test")) {//true branch
+			tmp_decisionNode[i + 1] = branchT[i]->FirstChildElement("test");//first test node
+			decisionNode.push_back(branchT[i]->FirstChildElement("test")->GetText());
+			//decision[i]->SetTrueNode(tmpT);
+			//decision[i]->SetFalseNode(tmpF);
+			branchT[i] = branchT[i]->FirstChildElement("true");
+			branchF[i] = branchT[i]->FirstChildElement("false");
+			tmpT = nullptr;
+			tmpF = nullptr;
+		}
+		else if (branchT[i]->FirstChild() == branchT[i]->FirstChildElement("action")) {
+			
+		}
+
+
+		decision.SetDecision(decisionNode);
+		i++;
+		counter++;
+	}
+	while (true) {
+		if (branchF[i]->FirstChild() == branchF[i]->FirstChildElement("test")) {
+			tmp_decisionNode[i + 1] = branchF[i]->FirstChildElement("test");
+		}
+		else if (branchF[i]->FirstChild() == branchF[i]->FirstChildElement("test")) {//true branch
+			tmp_decisionNode[i + 1] = branchF[i]->FirstChildElement("test");//first test node
+			decisionNode.push_back(branchF[i]->FirstChildElement("test")->GetText());
+			//decision[i]->SetTrueNode(tmpT);
+			//decision[i]->SetFalseNode(tmpF);
+			branchT[i] = branchT[i]->FirstChildElement("true");
+			branchF[i] = branchT[i]->FirstChildElement("false");
+			tmpT = nullptr;
+			tmpF = nullptr;
+		}
+	}
+
+
+	return decision;
 }
